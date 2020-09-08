341606661c9f20af87861b97e644135442223443
claudiuintech <moisaclaudiu2309@gmail.com>
2020-09-08 20:42:22 +0300

    project starter

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import Layout from './components/Layout'
+import KanbanBoard from './components/KanbanBoard'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-	const [data, setData] = useState([]);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			const result = await axios('http://127.0.0.1:8080/api/users');
-
-			setData(result.data);
-		};
-
-		fetchData();
-	}, []);
-
-	console.log(data);
 
 	return (
-		<div className="app">
-			{data.map((user) => (
-				<div key={user.ID}>
-					<h2>Logged user: {user.name}</h2>
-					<div>
-						{user.Issues.map((issue) => (
-							<div
-								key={issue.ID}
-								style={{
-									display: 'flex',
-									justifyContent: 'center',
-									alignItems: 'center',
-									flexDirection: 'column',
-									border: '1px solid #ccc',
-								}}>
-								<p>Issue title: {issue.title}</p>
-								<p>Description: {issue.description}</p>
-								<p>CreatedAt: {issue.CreatedAt}</p>
-								<p>Id: {issue.ID}</p>
-								<p>Reporter: {issue.reporter}</p>
-								<p>Estimated: {issue.estimated}</p>
-								<p>Timelog: {issue.timelog}</p>
-								<p>Progress: {issue.progress}</p>
-								<p>Priority: {issue.priority}</p>
-								<div>
-									<h3>Comments:</h3>
-									{issue.Comments.map((comment) => (
-										<div key={comment.ID}>
-											<p>{comment.content}</p>
-										</div>
-									))}
-								</div>
-							</div>
-						))}
-					</div>
-				</div>
-			))}
-		</div>
+
+		<Router>
+			<Layout>
+				<Switch>
+					<Route
+						exact
+						path="/"
+						component={KanbanBoard}
+
+					/>
+				</Switch>
+			</Layout>
+		</Router>
 	);
 }
 
---
+++ client/src/components/KanbanBoard.jsx
@@ -0,0 +1,60 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const KanbanBoard = () => {
+    const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios('http://127.0.0.1:8080/api/users');
+
+			setData(result.data);
+		};
+
+		fetchData();
+	}, []);
+
+	console.log(data);
+    return (
+        <div>
+            {data.map((user) => (
+				<div key={user.ID}>
+					<h2>Logged user: {user.name}</h2>
+					<div>
+						{user.Issues.map((issue) => (
+							<div
+								key={issue.ID}
+								style={{
+									display: 'flex',
+									justifyContent: 'center',
+									alignItems: 'center',
+									flexDirection: 'column',
+									border: '1px solid #ccc',
+								}}>
+								<p>Issue title: {issue.title}</p>
+								<p>Description: {issue.description}</p>
+								<p>CreatedAt: {issue.CreatedAt}</p>
+								<p>Id: {issue.ID}</p>
+								<p>Reporter: {issue.reporter}</p>
+								<p>Estimated: {issue.estimated}</p>
+								<p>Timelog: {issue.timelog}</p>
+								<p>Progress: {issue.progress}</p>
+								<p>Priority: {issue.priority}</p>
+								<div>
+									<h3>Comments:</h3>
+									{issue.Comments.map((comment) => (
+										<div key={comment.ID}>
+											<p>{comment.content}</p>
+										</div>
+									))}
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			))}
+        </div>
+    )
+}
+
+export default KanbanBoard
