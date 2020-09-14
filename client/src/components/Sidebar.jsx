d74998f2887ec7c9299040c0f62b5c0b78f31599
Daniel Moisa <danimoisa93@gmail.com>
2020-09-14 17:39:05 +0300

    added dynamic user and projects

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Link, NavLink  } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 import axios from 'axios';
 import './Sidebar.scss';
@@ -14,44 +14,83 @@ import {
 import { GrClose } from 'react-icons/gr';
 
 const Sidebar = () => {
-	
-    const [userInfo, setUserInfo] = useState(false)
-    const [searchIssues, setSearchIssues] = useState(false)
+	const [userInfo, setUserInfo] = useState(false);
+	const [searchIssues, setSearchIssues] = useState(false);
+	const [data, setData] = useState([]);
 
-    return (
-        <>
-            <div className={`account ${ userInfo ?  'dropdown-active' : '' }`} onClick={ () => setUserInfo(!userInfo) }>
-                <div className="account-wrapper">
-                    <div className="avatar">
-                        <div className="info center">
-                            <FcTrademark />
-                            <p>User name</p>
-                        </div>
-                        { userInfo ?  <BsChevronUp/> : <BsChevronDown /> } 
-                    </div>
-                </div>
-            </div>
-            { userInfo &&
-                <div className="account-dropdown">
-                    <a>Item 1</a>
-                    <a>Item 2</a>
-                    <a>Item 3</a>
-                </div>
-            }
-            <NavLink exact to="/" activeClassName="active-link" className="sidebar-item board">
-                <BsGrid3X3 />
-                <span>Kanban Board</span>
-            </NavLink>
-            <div className={`sidebar-item search-issues ${ searchIssues ?  'dropdown-active' : '' }`} onClick={ () => setSearchIssues(!searchIssues) }>
-                {searchIssues ? <div><GrClose /> <span>Close search</span></div> : <div><BsSearch /> <span>Search issues</span></div> }
-            </div>
-            <div className="search-dropdown">
-                { searchIssues && 
-                    <input type="text" autoFocus placeholder="Search issues here..."/>
-                }
-            </div>
-        </>
-    )
-}
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios('http://127.0.0.1:8080/api/users/1');
 
-export default Sidebar
+			setData(result.data);
+		};
+
+		fetchData();
+	}, []);
+
+	console.log(data);
+
+	return (
+		<>
+			<div
+				className={`account ${userInfo ? 'dropdown-active' : ''}`}
+				onClick={() => setUserInfo(!userInfo)}>
+				<div className="account-wrapper">
+					<div className="avatar">
+						<div className="info center">
+							<FcTrademark />
+							<p>{data.name}</p>
+						</div>
+						{userInfo ? <BsChevronUp /> : <BsChevronDown />}
+					</div>
+				</div>
+			</div>
+			{userInfo && (
+				<div className="account-dropdown">
+					<a>Item 1</a>
+					<a>Item 2</a>
+					<a>Item 3</a>
+				</div>
+			)}
+			<NavLink
+				exact
+				to="/"
+				activeClassName="active-link"
+				className="sidebar-item board">
+				<BsGrid3X3 />
+				<span>Kanban Board</span>
+			</NavLink>
+			<div
+				className={`sidebar-item search-issues ${
+					searchIssues ? 'dropdown-active' : ''
+				}`}
+				onClick={() => setSearchIssues(!searchIssues)}>
+				{searchIssues ? (
+					<div>
+						<GrClose /> <span>Close search</span>
+					</div>
+				) : (
+					<div>
+						<BsSearch /> <span>Search issues</span>
+					</div>
+				)}
+			</div>
+			<div className="search-dropdown">
+				{searchIssues && (
+					<input
+						type="text"
+						autoFocus
+						placeholder="Search issues here..."
+					/>
+				)}
+			</div>
+
+			<div className="projects">
+				{data.Projects &&
+					data.Projects.map((project) => <p>{project.name}</p>)}
+			</div>
+		</>
+	);
+};
+
+export default Sidebar;
