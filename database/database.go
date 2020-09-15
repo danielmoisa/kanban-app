a2954a64e26c78012092eae036f745b3c1130b25
Daniel Moisa <danimoisa93@gmail.com>
2020-09-15 21:03:48 +0300

    db moved to database folder

--- database/database.go
+++ database/database.go
@@ -1,9 +1,23 @@
 package database
 
 import (
+	"fmt"
+	"jira-clone/models"
+
 	"github.com/jinzhu/gorm"
 )
 
 var (
 	DBConn *gorm.DB
 )
+
+func InitDatabase() {
+	var err error
+	DBConn, err = gorm.Open("sqlite3", "database.db")
+	if err != nil {
+		panic("Failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+	DBConn.AutoMigrate(&models.Project{}, &models.Issue{}, &models.User{}, &models.Comment{})
+	fmt.Println("Database Migrated")
+}
