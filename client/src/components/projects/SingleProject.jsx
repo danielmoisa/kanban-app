6e36a2fe6477b85a46e185eb626bb7ef773327eb
claudiuintech <moisaclaudiu2309@gmail.com>
2020-09-19 13:02:22 +0300

    added kanban board v2 & sidebar projects search

---
+++ client/src/components/projects/SingleProject.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const SingleProject = ({ match }) => {
+    const [project, setProject] = useState();
+    const {
+        params: { projectId },
+    } = match;
+
+    useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios(`http://localhost:8080/api/projects/${projectId}`);
+
+            setProject(result.project);
+		};
+
+		fetchData();
+	}, [projectId]);
+
+    return (
+        <div className="single-project-wrapper">
+           <h1>{ project.name }</h1>
+        </div>
+    )
+}
+
+export default SingleProject
