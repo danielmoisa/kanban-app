15f47f1c7a4cc5eff2fda0d14a05d76143f6f397
Daniel Moisa <danimoisa93@gmail.com>
2020-10-03 18:09:57 +0300

    clean up board

--- client/src/App.js
+++ client/src/App.js
@@ -2,53 +2,34 @@ import React, { useState } from 'react';
 import './App.scss';
 import axios from 'axios';
 
-import Layout from './components/Layout'
-import KanbanBoard from './components/KanbanBoard'
-import TasksBoard from './components/TasksBoard'
-import TasksBoard2 from './components/TasksBoard2'
-import AddTask from './components/AddTask'
-import SingleProject from './components/projects/SingleProject'
-import SingleIssue from './components/issues/SingleIssue'
-import Login from './components/Login'
+import Layout from './components/Layout';
+import KanbanBoard from './components/KanbanBoard';
+import TasksBoard from './components/TasksBoard';
+import AddTask from './components/AddTask';
+import SingleProject from './components/projects/SingleProject';
+import SingleIssue from './components/issues/SingleIssue';
+import Login from './components/Login';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-
 	return (
 		<Router>
 			<Layout>
 				<Switch>
-					<Route
-						exact
-						path="/"
-						component={TasksBoard}
-					/>
-					<Route
-						exact
-						path="/add-task"
-						component={AddTask}
-					/>
-					<Route
-						exact
-						path="/v2"
-						component={TasksBoard2}
-					/>
+					<Route exact path="/" component={TasksBoard} />
+					<Route exact path="/add-task" component={AddTask} />
 					<Route
 						exact
 						path="/project/:projectId"
 						component={SingleProject}
 					/>
-					<Route 
-						exact 
+					<Route
+						exact
 						path="/issue/:issueId"
 						component={SingleIssue}
 					/>
-					<Route
-						exact
-						path="/login"
-						component={Login}
-					/>
+					<Route exact path="/login" component={Login} />
 				</Switch>
 			</Layout>
 		</Router>
--- client/src/components/TasksBoard.jsx
+++ client/src/components/TasksBoard.jsx
@@ -1,142 +1,142 @@
-import React, { useState, useCallback, useRef, useEffect } from 'react';
-import { DndProvider, useDrag, useDrop } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import update from 'immutability-helper';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 import axios from 'axios';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import NaturalDragAnimation from 'natural-drag-animation-rbdnd';
+
+import { BsPlus } from 'react-icons/bs';
 
 import './TasksBoard.scss';
 
-const channels = ['backlog', 'new', 'In progress', 'Done'];
+const TasksBoard = () => {
+	const issues = [
+		{ id: 'task-1', content: 'First task', status: 'Requested' },
+		{ id: 'task-2', content: 'Second task', status: 'To do' },
+		{ id: 'task-3', content: 'Third task', status: 'In progress' },
+		{ id: 'task-4', content: 'Fourth task', status: 'Done' },
+		{ id: 'task-5', content: 'Fifth task', status: 'Requested' },
+	];
 
-const labelsMap = {
-	backlog: 'Backlog',
-	new: 'To Do',
-	'In progress': 'In Progress',
-	Done: 'Done',
-};
-
-const classes = {
-	board: {
-		display: 'flex',
-		margin: '0 auto',
-	},
-	column: {
-		minWidth: 200,
-		width: '18vw',
-		height: '80vh',
-		margin: '0 auto',
-		backgroundColor: '#FCC8B2',
-		border: '1px solid #fff',
-	},
-	columnHead: {
-		textAlign: 'center',
-		padding: 10,
-		fontSize: '1.2em',
-		backgroundColor: '#C6D8AF',
-	},
-	item: {
-		padding: 10,
-		margin: 10,
-		fontSize: '0.8em',
-		cursor: 'pointer',
-		backgroundColor: 'white',
-	},
-};
-
-const Kanban = () => {
-	const [tasks, setTaskStatus] = useState();
-
-	useEffect(() => {
-		const fetchData = async () => {
-			const result = await axios('http://127.0.0.1:8080/api/projects/1');
-
-			setTaskStatus(result.data.Issues);
-		};
-
-		fetchData();
-	}, []);
-
-	const changeTaskStatus = useCallback(
-		(id, progress) => {
-			let task = tasks.find((task) => task.ID === id);
-			const taskIndex = tasks.indexOf(task);
-			task = { ...task, progress };
-			let newTasks = update(tasks, {
-				[taskIndex]: { $set: task },
-			});
-			setTaskStatus(newTasks);
+	const cols = [
+		{
+			name: 'Requested',
 		},
-		[tasks]
-	);
+
+		{
+			name: 'To do',
+		},
+
+		{
+			name: 'In Progress',
+		},
+		{
+			name: 'Done',
+		},
+	];
+
+	const [columns, setColumns] = useState(cols);
+	const [data, setData] = useState(issues);
+
+	const onDragEnd = (result, columns, setColumns, data, setData) => {};
 
 	return (
-		<main>
-			<DndProvider backend={HTML5Backend}>
-				<section className="board-columns">
-					{channels.map((channel) => (
-						<KanbanColumn
-							key={channel}
-							status={channel}
-							changeTaskStatus={changeTaskStatus}>
-							<div className="board-single-column">
-								<h3>{labelsMap[channel]}</h3>
-
-								<div>
-									{tasks &&
-										tasks
-											.filter(
-												(item) =>
-													item.progress === channel
-											)
-											.map((item) => (
-												<KanbanItem
-													key={item.ID}
-													id={item.ID}>
-													<div className="column-tasks-wrapper">
-														<div className="task-card">
-															{item.title} <br />
-															{item.description}
-														</div>
+		<>
+			<div className="board-columns">
+				<DragDropContext
+					onDragEnd={(result) =>
+						onDragEnd(result, columns, setColumns)
+					}>
+					{cols.map((singleCol) => (
+						<div
+							className="board-single-column"
+							key={singleCol.name}>
+							<h3>{singleCol.name}</h3>
+							<Droppable droppableId={singleCol.name}>
+								{(provided, snapshot) => {
+									return (
+										<div
+											className="column-tasks-wrapper"
+											{...provided.droppableProps}
+											ref={provided.innerRef}
+											style={{
+												backgroundColor: snapshot.isDraggingOver
+													? '#afb8ea'
+													: '#e0e3f5',
+												padding: 10,
+												minHeight: 500,
+												boxShadow: snapshot.isDraggingOver
+													? '0px 2px 7px 1px rgba(0, 0, 0, 0.1)'
+													: 'none',
+											}}>
+											{issues.map((item, index) => {
+												return (
+													<div key={item.id}>
+														{item.status ===
+															singleCol.name && (
+															<Draggable
+																draggableId={
+																	item.id
+																}
+																index={index}>
+																{(
+																	provided,
+																	snapshot
+																) => (
+																	<NaturalDragAnimation
+																		style={
+																			provided
+																				.draggableProps
+																				.style
+																		}
+																		snapshot={
+																			snapshot
+																		}>
+																		{(
+																			style
+																		) => (
+																			<div
+																				className="task-card"
+																				ref={
+																					provided.innerRef
+																				}
+																				{...provided.draggableProps}
+																				{...provided.dragHandleProps}
+																				style={{
+																					userSelect:
+																						'none',
+																					padding: 16,
+																					margin:
+																						'0 0 8px 0',
+																					minHeight:
+																						'50px',
+																					backgroundColor: snapshot.isDragging
+																						? '#f5f6f8'
+																						: '#fff',
+																					...style,
+																				}}>
+																				{
+																					item.content
+																				}
+																			</div>
+																		)}
+																	</NaturalDragAnimation>
+																)}
+															</Draggable>
+														)}
 													</div>
-												</KanbanItem>
-											))}
-								</div>
-							</div>
-						</KanbanColumn>
+												);
+											})}
+											{provided.placeholder}
+										</div>
+									);
+								}}
+							</Droppable>
+						</div>
 					))}
-				</section>
-			</DndProvider>
-		</main>
+				</DragDropContext>
+			</div>
+		</>
 	);
 };
-
-export default Kanban;
-
-const KanbanColumn = ({ status, changeTaskStatus, children }) => {
-	const ref = useRef(null);
-	const [, drop] = useDrop({
-		accept: 'card',
-		drop(item) {
-			changeTaskStatus(item.id, status);
-		},
-	});
-	drop(ref);
-	return <div ref={ref}> {children}</div>;
-};
-
-const KanbanItem = ({ id, children }) => {
-	const ref = useRef(null);
-	const [{ isDragging }, drag] = useDrag({
-		item: { type: 'card', id },
-		collect: (monitor) => ({
-			isDragging: monitor.isDragging(),
-		}),
-	});
-	const opacity = isDragging ? 0 : 1;
-	drag(ref);
-	return (
-		<div ref={ref} style={{ opacity }}>
-			{children}
-		</div>
-	);
-};
+export default TasksBoard;
