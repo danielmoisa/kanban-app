a794862fa49b9df9eeea18b5e9cbe4a92f66029e
claudiuintech <moisaclaudiu2309@gmail.com>
2020-10-18 18:11:22 +0300

    add comment & design

---
+++ client/src/components/comments/comments.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const url = 'http://127.0.0.1:8080/api/comments'
+
+const getComments = () => {
+    return axios.get(url)
+}
+
+const addComment = (newComment) => {
+    return axios.post(url, newComment)
+}
+
+const deleteComment = (commentId) => {
+    const request = axios.delete(`${url}/${commentId}`);
+    return request.then(response => response.data)
+}
+
+const updateComment = (id, newUpdatedComment) => {
+    const request = axios.patch(`${url}/${id}`, newUpdatedComment)
+    return request.then( response => response.data)
+}
+
+export default { getComments, addComment, deleteComment, updateComment }
