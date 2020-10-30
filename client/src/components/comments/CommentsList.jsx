44a3261406e753ea97fcbb4d696f3405484abc13
Daniel Moisa <danimoisa93@gmail.com>
2020-10-30 20:29:20 +0200

    static url remove

--- client/src/components/comments/CommentsList.jsx
+++ client/src/components/comments/CommentsList.jsx
@@ -1,105 +1,117 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
-import services from './comments';
+import services from "./comments";
 
 //Notification box
 import { toast } from "react-toastify";
 
 //Icons
-import { AiOutlineDelete } from 'react-icons/ai';
+import { AiOutlineDelete } from "react-icons/ai";
 
 const CommentsList = ({ issue, issueId }) => {
-    const [comment, setComment] = useState('');
-    const [existingComments, setExistingComments] = useState('');
+	const [comment, setComment] = useState("");
+	const [existingComments, setExistingComments] = useState("");
 
-    //Fetching single issue comments
+	//Fetching single issue comments
 	useEffect(() => {
 		const fetchData = async () => {
-			const result = await axios(
-				`http://localhost:8080/api/issues/${issueId}`
-			);
+			const result = await axios(`/api/issues/${issueId}`);
 
 			setExistingComments(result.data.Comments);
 		};
 
-        fetchData();
+		fetchData();
 	}, [issueId]);
 
-    //Add new comment
-    const addNewComment = e => {
-        e.preventDefault()
+	//Add new comment
+	const addNewComment = (e) => {
+		e.preventDefault();
 
-        if(comment !== "" && comment.length > 5) {
-          const newComment = {
-            content: comment,
-            IssueID: issue.ID
-          }
-          services.addComment(newComment).then(response => {
-            setExistingComments(existingComments.concat(response.data))
-            setComment("")
-          })
-          toast.success("Comment successfully added!");
-        } else {
-            toast.error("Add at least 6 characters!");
-        }
+		if (comment !== "" && comment.length > 5) {
+			const newComment = {
+				content: comment,
+				IssueID: issue.ID,
+			};
+			services.addComment(newComment).then((response) => {
+				setExistingComments(existingComments.concat(response.data));
+				setComment("");
+			});
+			toast.success("Comment successfully added!");
+		} else {
+			toast.error("Add at least 6 characters!");
+		}
+	};
 
-    }
+	//Close add comment
+	const closeAddComment = () => {
+		setComment("");
+	};
 
-    //Close add comment
-    const closeAddComment = () => {
-        setComment('')
-    }
+	//Delete comment
+	const deleteSingleComment = (commentId) => {
+		services.deleteComment(commentId).then((response) => {
+			const updatedCommentsList = existingComments.filter(
+				(comment) => comment.ID !== commentId
+			);
+			setExistingComments(updatedCommentsList);
+		});
+		toast.success("Comment successfully deleted");
+	};
 
-    //Delete comment
-    const deleteSingleComment = (commentId) => {
-        
-        services.deleteComment(commentId).then(response => {
-            const updatedCommentsList = existingComments.filter(comment => comment.ID !== commentId)
-            setExistingComments(updatedCommentsList);
-          })
-		toast.success('Comment successfully deleted');
-    };
-    
+	return (
+		<>
+			<div className="comments-wrapper">
+				<h4>Issue comments</h4>
+				<div className="add-comment-input">
+					<textarea
+						name="content"
+						placeholder="Add new comment..."
+						value={comment}
+						onChange={(e) => setComment(e.target.value)}
+					/>
+					{comment.length > 0 ? (
+						<div className="buttons">
+							<button
+								className="secondary-btn"
+								onClick={closeAddComment}>
+								Cancel
+							</button>
+							<button
+								className="primary-btn"
+								onClick={addNewComment}>
+								Add
+							</button>
+						</div>
+					) : (
+						""
+					)}
+				</div>
+				{existingComments &&
+					existingComments.map((singleComment) => (
+						<div className="single-comment" key={singleComment.ID}>
+							<div className="left">
+								<div className="author">
+									<h5>John Doe</h5>
+									<span>3 days ago</span>
+								</div>
 
-    return (
-        <>
-            <div className="comments-wrapper">
-                 <h4>Issue comments</h4>
-                <div className="add-comment-input">
-                    <textarea name="content" placeholder="Add new comment..." 
-                        value={comment} 
-                        onChange={ e => setComment(e.target.value) }
-                    />
-                    { comment.length > 0 ?
-                        <div className="buttons">
-                            <button className="secondary-btn" onClick={closeAddComment}>Cancel</button>
-                            <button className="primary-btn" onClick={addNewComment}>Add</button>
-                        </div>
-                    :
-                    ''
-                    }
-                </div>
-                {existingComments && existingComments.map((singleComment) => (
-                    <div className="single-comment" key={singleComment.ID}>
-                      <div className="left">
-                        <div className="author">
-                                <h5>John Doe</h5>
-                                <span>3 days ago</span>
-                            </div>
-                          
-                                <p>{singleComment.content}</p>
-                            
-                        </div>
-                        <div className="right">
-                            <AiOutlineDelete className="delete" onClick={ () => deleteSingleComment(singleComment.ID) }/>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </>
-    )
-}
+								<p>{singleComment.content}</p>
+							</div>
+							<div className="right">
+								<AiOutlineDelete
+									className="delete"
+									onClick={() =>
+										deleteSingleComment(singleComment.ID)
+									}
+								/>
+							</div>
+						</div>
+					))}
+			</div>
+		</>
+	);
+};
 
-export default CommentsList
+export default CommentsList;
